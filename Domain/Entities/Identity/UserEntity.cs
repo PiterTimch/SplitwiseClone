4356a54b93094a4435f33b7e87fc5c6fb362eb24
Petro <petrotymchuck@gmail.com>
2026-09-23 18:55:07 +0300

    Refactor UserEntity to inherit from IdentityUser<long>

--- Domain/Entities/Identity/UserEntity.cs
+++ Domain/Entities/Identity/UserEntity.cs
@@ -1,8 +1,13 @@
-﻿using SplitwiseClone.Domain.Entities.Additional;
+﻿using Microsoft.AspNetCore.Identity;
+using SplitwiseClone.Domain.Entities.Additional;
 
 namespace SplitwiseClone.Domain.Entities.Identity;
 
-public class UserEntity : BaseEntity<long>
+public class UserEntity : IdentityUser<long>
 {
+    public string? FirstName { get; set; } = null;
+    public string? LastName { get; set; } = null;
 
+    public DateTime DateCreated { get; set; } = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
+    public bool IsDeleted { get; set; }
 }
